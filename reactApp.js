a8e41341ce44f8212ead4dd363fc7242f3b77043
tysherman <plankquanta@gmail.com>
2020-11-09 01:27:57 -0800

    title and h1 changed; one image rendered

--- reactApp.js
+++ reactApp.js
@@ -1,15 +1,18 @@
 // Deafault App component that all other compents are rendered through
-function App(props){
+function App(props) {
   return (
     <div>
-      <h1>Welcome to the sports game starter</h1>
-      This file represents the code after completing the setup step in the lab instructions
+      <h1>Welcome to the British and Irish Quidditch League Cup!</h1>
+      <br />
+      <img src="images\chudley.gif" />
+      {/* <br />
+      <h2>Vs.</h2>
+       */}
+      <br />
+      <img scr="images\holyhead.gif" />
     </div>
-  )
+  );
 }
 
 //Render the application
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
